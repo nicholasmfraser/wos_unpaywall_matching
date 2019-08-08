99bfbb2e90a42fce31f465fe33f86a3fedc37f3e
nicholasmfraser <nicholasmfraser@gmail.com>
2019-08-08 13:11:18 +0200

    initial commit

---
+++ sql/create_unpaywall_issns.sql
@@ -0,0 +1,17 @@
+CREATE TABLE UPW_14_NORM_ISSNS AS WITH UPW_ISSNS AS (select 
+    DOI,
+    regexp_substr(JOURNAL_ISSNS, '[^,]+', 1, 1) as ISSN_1, 
+    regexp_substr(JOURNAL_ISSNS, '[^,]+', 1, 2) as ISSN_2,
+    regexp_substr(JOURNAL_ISSNS, '[^,]+', 1, 3) as ISSN_3,
+    regexp_substr(JOURNAL_ISSNS, '[^,]+', 1, 4) as ISSN_4
+FROM
+    UPW_14_NORM)
+select distinct
+    DOI, ISSN_VALUE
+FROM
+    UPW_ISSNS
+unpivot
+(
+  ISSN_VALUE
+  for ISSN_NAME in (ISSN_1, ISSN_2, ISSN_3, ISSN_4)
+);
